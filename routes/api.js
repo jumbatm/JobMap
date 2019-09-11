ae7d54b801d151ad8fe6850c7a6761b42fc34712
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-09-11 21:07:56 +1000

    Pass processed listings to the client

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,15 @@
-
+"use strict";
 const express = require('express');
 const router = express.Router();
 const geo = require('../lib/geo');
 const job = require('../lib/job');
-const Point = require('../lib/Point');
 const ors = require('../lib/ors');
 
 class ProcessedListing extends job.Job {
-  constructor(name, position, description, suburb, time) {
+  constructor(name, position, description, suburb, lat, lng, time) {
     super(name, position, description, suburb);
+    this._lat = lat;
+    this._lng = lng;
     this._time = time;
   }
   getTime() { return this._time; }
@@ -20,25 +21,39 @@ router.get('/', function (req, res, _) {
       let point = values[0];
 
       let listings_promises = values[1].map(l => {
-        geo.geocode(l.getSuburb()).then(value => { 
-          ors.routeBetween(
-            process.env.ORS_API_KEY, 
-            point,
-            value)
-          .then(value => { console.log(value.data.routes); })
-          .catch(e => console.log(e));
-        })
-        .catch(e => console.log(e));
-      });
+        return new Promise(
+          (resolve, reject) => { 
+            geo.geocode(l.getSuburb()).then(value => { 
+              ors.routeBetween(
+                process.env.ORS_API_KEY, 
+                point,
+                value)
+                .then(value => {
+                  let time = value.data.routes[0].summary.duration;
+                  console.log(time);
+                  resolve(new ProcessedListing(
+                    l.getName(),
+                    l.getPosition(), 
+                    l.getDesc(), 
+                    l.getSuburb(),
+                    time
+                  ));
+                }).catch(e => reject(e));
+            }).catch(e => reject(e))/* goecode*/
+          }
+        )});
 
+      console.log(listings_promises);
       Promise.all(listings_promises).then(listings => {
+        console.log("Listings:");
+        console.log(listings);
         res.json({
           lat: point.getLatitude(),
           lon: point.getLongitude(),
           listings: listings,
         });
       })
-      .catch(e => console.log(e));
+        .catch(e => console.log(e));
 
     })
     .catch(function(e) { console.log(e); });
