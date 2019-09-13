d1a24fdff7cab0dd48e3d018ed8598b42e0f04be
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-09-13 20:31:19 +1000

    Use matrix API to bulk-search.

--- routes/api.js
+++ routes/api.js
@@ -24,103 +24,70 @@ class ProcessedListing extends job.Job {
 router.get('/', function (req, res, _) {
   Promise.all([ geo.geocode(req.query.address), job.getJobs(req.query.keywords, req.query.address)])
     .then(values => {
-      let point = values[0];
+      let homePoint = values[0];
 
-      console.log(values[1]);
-
-      let listings_promises = values[1].map(l => {
-        return new Promise(
-          (resolve, reject) => { 
-            geo.geocode(l.getSuburb()).then(suburbPoint => { 
-                ors.routeBetween(
-                  process.env.ORS_API_KEY, 
-                  point,
-                  suburbPoint)
-                  .then(value => {
-                    let time = value.data.routes[0].summary.duration;
-                    resolve(new ProcessedListing(
-                      l.getName(),
-                      l.getPosition(), 
-                      l.getDesc(), 
-                      l.getSuburb(),
-                      suburbPoint.getLatitude(),
-                      suburbPoint.getLongitude(),
-                      time
-                    ));
-                  }).catch(e => reject(e));
-              }).catch(e => reject(e))
-          }
-        )});
-
-      Promise.all(listings_promises).then(listings => {
-        listings = listings.sort((x, y) => {
-          // Sort by GPS coordinate.
-          let xp = x.getLatitude() + "#" + x.getLongitude();
-          let yp = y.getLatitude() + "#" + y.getLongitude();
-          if (xp === yp) {
-            return 0;
-          }
-          if (xp < yp) {
-            return 1;
-          }
-          if (xp > yp) {
-            return -1;
-          }
+      let uniquedSuburbs = [...new Set(values[1].map(l => l.getSuburb()))];
+      let uniqueGeocodeWithPromise = 
+        uniquedSuburbs.map(suburbName => {
+          return geo.geocode(suburbName);
         });
 
+      Promise.all(uniqueGeocodeWithPromise).then(points => {
 
-        // We now merge listings with the same lat/longitude.
-        let markers = listings.reduce((acc, current) => {
-          let toJSON = function(jobListing) {
-            return {
-                name: jobListing.getName(),
-                position: jobListing.getPosition(),
-                description: jobListing.getDesc(),
-            };
-          };
-          let prev = acc.length !== 0 ? acc[acc.length-1] : undefined;
-          // acc: The new list.
-          if (acc.length !== 0 
-            && prev.lat === current.getLatitude() 
-            && prev.lng === current.getLongitude()) {
-            // The values are the same. Therefore, we merge with the previous node.
-            prev.jobs.push(toJSON(current));
-          } else {
-            // Values are unique. We append current to acc.
-            acc.push({
-              lat: current.getLatitude(),
-              lng: current.getLongitude(),
-              suburb: current.getSuburb(),
-              time: current.getTime(),
-              jobs: [toJSON(current)],
+        // Now, fire off the bulk request for time taken for points.
+        ors.bulkRouteBetween(process.env.ORS_API_KEY, 
+          [homePoint.getLongitude(), homePoint.getLatitude()],
+          points.map(v => { return [ v.getLongitude(), v.getLatitude() ]; }),
+        ).then(response => {
+          const durations = response.durations;
+
+          let result = {};
+          points.forEach((point, i) => {
+            const suburbName = uniquedSuburbs[i];
+            const duration = durations[0][i];
+            result[suburbName] = { lat: point.getLatitude(), lng: point.getLongitude(), duration: duration, };
+          });
+
+          // Now, we iterate through our jobs, and using the location of the job as a key, we append job information.
+          values[1].forEach(job => {
+            result[job.getSuburb()]["jobs"] = result[job.getSuburb()]["jobs"] || [];
+            result[job.getSuburb()]["jobs"].push({
+              name: job.getName(),
+              position: job.getPosition(),
+              description: job.getDesc(),
             });
-          }
-          return acc;
-        }, []);
+          });
 
-        // Provide the full HTML for each of the markers..  
-        let rendered = markers.map((value) => {
-          return {
-            lat: value.lat,
-            lng: value.lng,
-            html: jobRenderer({
-              jobs: value.jobs,
-              suburb: value.suburb,
-              time: value.time,
-            }),
-          }
-        });
+          Object.freeze(result);
+          console.log(result);
 
-        res.json({
-          from: {
-            lat: point.getLatitude(),
-            lon: point.getLongitude(),
-          },
-          markers: rendered,
-        });
+          // Provide the full HTML for each of the markers.
+          let rendered = Object.keys(result).map((suburbName) => {
+            return {
+              lat: result[suburbName].lat,
+              lng: result[suburbName].lng,
+              html: jobRenderer({
+                jobs: result[suburbName].jobs,
+                suburb: result[suburbName].suburb,
+                time: result[suburbName].duration,
+              }),
+            };
+          });
+
+          res.json({
+            from: {
+              lat: homePoint.getLatitude(),
+              lon: homePoint.getLongitude(),
+            },
+            markers: rendered,
+          });
+        }).catch(e => console.log(e));
+
       }).catch(e => console.log(e));
 
-    }).catch(function(e) { console.log(e); });
+
+    }).catch(e => console.log(e));
+
 });
 
 module.exports = router;
